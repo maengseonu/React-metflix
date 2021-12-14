acc2535b12138bfac959556b9c1b8419f87c7e7f
maengseonu <aodtjsdn123@gmail.com>
2021-12-14 18:07:56 +0900

    Home Screen part One(API-fetcher)

---
+++ src/api.ts
@@ -0,0 +1,8 @@
+const API_KEY = "5147aba9ac8131ceeeb66a50cd9e6fed";
+const BASE_PATH = "https://api.themoviedb.org/3";
+
+export function getMovies() {
+  return fetch(`${BASE_PATH}/movie/now_playing?api_key=${API_KEY}`).then(
+    (response) => response.json()
+  );
+}
